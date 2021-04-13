0db9a3da6717b45f0902063b78778ba351beb6c3
mohamedelkhamesgaied@esprit.tn <mohamedelkhames.gaied@esprit.tn>
2021-04-13 15:19:51 +0100

    deuxieme commit classe Employe

--- src/main/java/tn/esprit/spring/entities/Employe.java
+++ src/main/java/tn/esprit/spring/entities/Employe.java
@@ -22,7 +22,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Employe implements Serializable {
-	//
+	////////
 	private static final long serialVersionUID = 1L;
 
 	@Id
